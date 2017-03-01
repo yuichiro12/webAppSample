1138dbe1c83ea9ab4521fd2658117c657d77593e
yuichiro12 <talcnrhyme@gmail.com>
2017-03-02 08:38:30 +0900

    secure avatar update

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,17 +13,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     # default
-    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
 
     # Rails.root/public/images/ 配下にファイルが配置される
-    "images"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
-    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-      "/images/" + [version_name, "default.png"].compact.join('_')
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
   end
 
   # Process files as they are uploaded:
@@ -48,11 +46,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    if original_filename.present?
-      model.name + ".jpg"
-    else
-      "default.jpg"
-    end
+    secure_token + ".jpg" if original_filename.present?
   end
 
+
+  protected
+  
+    def secure_token
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    end
 end
